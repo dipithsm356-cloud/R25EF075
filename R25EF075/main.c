f420ec378e5f86a0eb4e1ccb8216fa05852f37a7
Dipithsm <dipithsm356@gmail.com>
2026-06-09 17:11:53 +0530

    u

--- R25EF075/main.c
+++ R25EF075/main.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 void main()
+void main()
+{
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+}
