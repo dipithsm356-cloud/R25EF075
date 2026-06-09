620a626e20766e2ff4ac3d46083bc3aeb9bf0edb
Dipithsm <dipithsm356@gmail.com>
2026-06-09 19:09:45 +0530

    King 5

--- R25EF075/main.c
+++ R25EF075/main.c
@@ -39,3 +39,23 @@ void rectangle(int x, int y, int w, int h)
         screen[i][x + w - 1] = '*';
     }
 }
+void line(int x1, int y1, int x2, int y2)
+{
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float Xinc = dx / (float)steps;
+    float Yinc = dy / (float)steps;
+
+    float x = x1;
+    float y = y1;
+
+    int i;
+    for(i = 0; i <= steps; i++)
+    {
+        screen[(int)(y + 0.5)][(int)(x + 0.5)] = '*';
+        x += Xinc;
+        y += Yinc;
+    }
+}
